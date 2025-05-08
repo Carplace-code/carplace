ac5dcd962eed058761b801bc0f44e5c00861d738
Naik <matas.seplveda@uc.cl>
2025-05-08 01:55:05 -0400

    feat: Añadida conexion con bdd

---
+++ app/api/brands/route.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  const brands = await prisma.brand.findMany();
+  return Response.json(brands);
+}
