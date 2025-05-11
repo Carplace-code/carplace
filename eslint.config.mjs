4c2d8ac7c03f18cecf111689e376ebac49b8a0f9
Naik <matas.seplveda@uc.cl>
2025-05-10 23:59:48 -0400

    feat: update prisma client version and add eslint ignore for generated files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,7 +25,7 @@ export default [
 
   // Main configuration block
   {
-    ignores: ["**/*.js", "**/__snapshots__/*"],
+    ignores: ["**/*.js", "**/__snapshots__/*", "prisma/app/generated/**"],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
