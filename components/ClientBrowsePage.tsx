4fc8472285a0befd676f218db8db03ab8cf96353
wintermelyn <matas.seplveda@uc.cl>
2025-06-08 23:57:03 -0400

    a

--- components/ClientBrowsePage.tsx
+++ components/ClientBrowsePage.tsx
@@ -23,7 +23,6 @@ export default function ClientBrowsePage({ brands }: Props) {
     modelId: "",
     minPrice: "",
     maxPrice: "",
-    location: "",
     transmission: "",
     fuel: "",
   });
